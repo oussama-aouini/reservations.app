8a7fb365011c3f6037de949b48438148fe08b7c9
Ousaama Aouini <oussama.aouini.dev@gmail.com>
2025-10-25 22:29:20 +0100

    add styling

--- app/index.tsx
+++ app/index.tsx
@@ -1,10 +1,27 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Image } from "react-native";
+import Logo from "../assets/img/oussama-aouini.jpg";
+
 const Home = () => {
   return (
-    <View>
-      <Text>Home</Text>
+    <View style={styles.container}>
+      {/* <Image source={Logo} /> */}
+      {/* here the first braces indicate a dynamic value the second ones are for the object syntax */}
+      <Text style={{ marginTop: 10, marginBottom: 15 }}>Inline syles</Text>
+      <Text style={[styles.title, {color: 'red'}]}>mutilpe styles</Text>
     </View>
   );
 };
+
 export default Home;
-const styles = StyleSheet.create({});
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  title: {
+    fontSize: 10,
+    fontWeight: "bold",
+  },
+});
